93942047e76078a2ad9ad0db30e331edd04c7bd0
mdphanba <USER@DOMAIN>
2009-05-09 02:27:06 +0000

    #381: Deal with XML-RPC nil values

--- tests/testxmlrpc_client/testserver.py
+++ tests/testxmlrpc_client/testserver.py
@@ -40,7 +40,29 @@ def echo(obj):
   return obj
 
 
+def hello_world():
+  """
+  <Purpose>
+    Serves as the no-args test function for the XML-RPC client.
+
+  <Arguments>
+    None.
+
+  <Exceptions>
+    None.
+
+  <Side Effects>
+    None.
+
+  <Returns>
+    "Hello World!"
+
+  """
+  return "Hello World!"
+
+
 if __name__ == "__main__":
   server = SimpleXMLRPCServer(("localhost", 8000))
   server.register_function(echo)
+  server.register_function(hello_world)
   server.serve_forever()
